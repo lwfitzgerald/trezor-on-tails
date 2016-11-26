4ae7f95d9140f8861f3f115c4ddf39c41a4f0564
Luke Fitzgerald <lw.fitzgerald@gmail.com>
2016-11-26 23:10:07 +0000

    Add udev rules

--- install-trezor.sh
+++ install-trezor.sh
@@ -5,4 +5,5 @@ sudo apt-get install -y --no-install-recommends \
 sudo torify pip install -U setuptools
 sudo torify pip install trezor==0.6.13
 sudo chmod -R ugo+rX /usr/local/lib/python2.7/dist-packages
+sudo sh -c 'curl -sL https://github.com/trezor/trezor-common/blob/master/udev/51-trezor.rules > /lib/udev/rules.d/51-trezor.rules'
 
