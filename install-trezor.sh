9dd245fce9ac74af709ab0e07ae513d8aa38e87c
Luke Fitzgerald <lw.fitzgerald@gmail.com>
2016-11-26 23:21:10 +0000

    Check running as root at the beginning so we can drop sudo everywhere

--- install-trezor.sh
+++ install-trezor.sh
@@ -1,10 +1,16 @@
 #!/bin/bash
-sudo apt-get update
-sudo apt-get install -y --no-install-recommends \
+
+if (( $EUID != 0 )); then
+    echo 'This script must be run as root. Please rerun using sudo.'
+    exit 1
+fi
+
+apt-get update
+apt-get install -y --no-install-recommends \
 	python-pip python-dev build-essential libusb-1.0-0-dev libudev-dev
-sudo torify pip install -U setuptools
-sudo torify pip install trezor==0.6.13
-sudo chmod -R ugo+rX /usr/local/lib/python2.7/dist-packages
-sudo sh -c 'curl -sL https://raw.githubusercontent.com/trezor/trezor-common/master/udev/51-trezor.rules > /lib/udev/rules.d/51-trezor.rules'
-sudo chmod 644 /lib/udev/rules.d/51-trezor.rules
+torify pip install -U setuptools
+torify pip install trezor==0.6.13
+chmod -R ugo+rX /usr/local/lib/python2.7/dist-packages
+sh -c 'curl -sL https://raw.githubusercontent.com/trezor/trezor-common/master/udev/51-trezor.rules > /lib/udev/rules.d/51-trezor.rules'
+chmod 644 /lib/udev/rules.d/51-trezor.rules
 
